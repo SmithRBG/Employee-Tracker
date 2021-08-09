d539810105a7cddaf22dbdaa57cce3f090b2903e
SmithRBG <smit36000@gmail.com>
2021-08-08 20:04:06 -0400

    updating update

--- index.js
+++ index.js
@@ -93,11 +93,15 @@ function addEmployee() {
         console.table(res);
         start();
     })};
+
+    //updating employees??
     
 function updateEmployee() {
   console.log("Updating Employees")
-  connection.query("",
+  connection.query("UPDATE Employee SET",
+    {
 
+    },
   function(err, res) {
     if(err) throw err;
     console.table(res);
@@ -105,51 +109,19 @@ function updateEmployee() {
   })};
 
 
+  function updateEmployee() {
+    if (err) throw err;
+    var sql = "UPDATE Employee SET role_id = '' WHERE Manager_id = ''";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+    });
+  };
+
 
       connection.connect((err) => {
         if(err) throw err;
         start();
     });
 
-
-
-
-
-
-
-
-
-   
-    
-
-
-
-
-
-
-
-/* const viewEmployee = () => {
-    console.log("Viewing employees\n");
-
-    var query =
-      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-    FROM employee e
-    LEFT JOIN role r
-      ON e.role_id = r.id
-    LEFT JOIN department d
-    ON d.id = r.department_id
-    LEFT JOIN employee m
-      ON m.id = e.manager_id`
-  
-    connection.query(query, function (err, res) {
-      if (err) throw err;
-  
-      console.table(res);
-      console.log("Employees viewed!\n");
-  
-      firstPrompt();
-    });
-    // console.log(query.sql);
-  
-} */
 
